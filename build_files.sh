ff7cc9e1038d76deab7171b2bcb4ddd67c9dbe40
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-06-05 18:52:32 +0000

    Update build_Files.sh file

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,10 @@
-source env/bin/activate
-pip install -r requirements.txt
-Python 3.10.12 manage.py collectstatic
+# Build the project
+echo "Building the project..."
+python3.10 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.10 manage.py collectstatic --noinput --clear
