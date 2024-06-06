ff1501e51c769fb9bc261e3bb6960cfbbc957579
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-06-06 18:31:31 +0000

    Updated build_files.sh file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
-pip3 install -r requirements.txt
-python3.9 manage.py collectstatic
+python -m venv venv
+source venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run your build commands (e.g., collectstatic for Django)
+python manage.py collectstatic --noinput
+
+# Deactivate the virtual environment
+deactivate
